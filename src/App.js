0aae8ab798d1afc60a5292cf1306824462222381
Ankit chauhan <127407236+Ankit1787@users.noreply.github.com>
2023-10-29 14:57:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home/Home.jsx"
-import Home2 from './pages/Home/Home2.jsx'
+
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
    <Router>
     <Routes>
         <Route  path='/'  element={<Home/>}/>
-        <Route  path='/me'  element={<Home2/>}/>
+      
     </Routes>
 
    </Router>
